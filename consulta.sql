b15e7a6101fd1ffdf7798fe75dda86fbc51f914d
Gabriel Pacheco Cortes da Silva <gabriel.pacheco.cs@gmail.com>
2024-02-01 14:49:24 -0300

    Update consulta.sql
    Acertando o acentos

--- consulta.sql
+++ consulta.sql
@@ -1,8 +1,8 @@
-SELECT d.Nome AS Departamento, p.Nome AS Colaborador, p.Salário
+SELECT d.Nome AS Departamento, p.Nome AS Colaborador, p.Salario
 FROM Pessoa p
 JOIN Departamento d ON p.DepId = d.Id
-WHERE (p.DepId, p.Salário) IN (
-    SELECT DepId, MAX(Salário)
+WHERE (p.DepId, p.Salario) IN (
+    SELECT DepId, MAX(Salario)
     FROM Pessoa
     GROUP BY DepId
 );
